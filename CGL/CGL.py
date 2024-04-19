47c9c7b382ae52ed0936fbdaf4b7d81c3423f0ab
BADBEEF6502 <165059888+BADBEEF6502@users.noreply.github.com>
2024-04-19 16:17:53 -0500

    Window stability and fixed shallow copy issue
    Simulator now uses window stability; run now accounts for int8 instead of int32 for colors; simulator had issue of shallow copying states which caused major issue with rendering and could cause unintentional damage. Now there is option to choose shallow copy (unsafe but speedy) or do deep copy (default/recommended).

--- CGL/CGL.py
+++ CGL/CGL.py
@@ -50,7 +50,7 @@ if GPU_CAPABLE:
 
 class sim:
     # Default to no state (1D or 2D numpy array for CGoL), side is the side length of the square, seed is used for random state generation, gpu chooses whether to use GPU or not, device selects the GPU device to use.
-    def __init__(self, state=None, side=8, seed=8, gpu=False, gpu_select=0, warp=8, spawnStabilityFactor=-1):
+    def __init__(self, state=None, side=8, seed=8, gpu=False, gpu_select=0, warp=8, spawnStabilityFactor=-1, stableStabilityFactor=1):
         # Validate input.
         if not isinstance(state, np.ndarray) and state is not None and not isinstance(state, list):
             raise TypeError('state variable must be a list or Numpy ndarray!')
@@ -74,6 +74,8 @@ class sim:
             raise ValueError('warp must be positive integer!')
         if not isinstance(spawnStabilityFactor, int):
             raise TypeError('spawnStabilityFactor must be an integer!')
+        if not isinstance(stableStabilityFactor, int):
+            raise TypeError('stableStabilityFactor must be an integer!')
         if GPU_CAPABLE != gpu:
             raise TypeError(f'the os enviornment variable "GPU_CAPABLE" (defaults to true) is {GPU_CAPABLE} and "gpu" is {gpu}. Both must be equal in value!\nTo launch use: GPU_CAPABLE="true\\false" python3 <script.py>')
 
@@ -83,6 +85,7 @@ class sim:
         self.count = 0
         self.seed = seed
         self.spawnStabilityFactor = spawnStabilityFactor
+        self.stableStabilityFactor = stableStabilityFactor
         self.gpu = gpu
 
         if state is not None:
@@ -101,7 +104,7 @@ class sim:
             self.world = np.random.randint(2, size=self.size, dtype=np.uint8)
 
         self.temp = np.empty_like(self.world)                       # Used here incase of forceCPU=True.
-        self.stable = np.zeros(self.size, dtype=np.int32)           # Used to store stable values for each cell, NOTE: IS SIGNED!
+        self.stable = np.zeros(self.size, dtype=np.int8)           # Used to store stable values for each cell, NOTE: IS SIGNED!
         self.stable[self.world != 0] = self.spawnStabilityFactor    # Every cell starts at the spawnStabilityFactor.
 
         # Setup CPU and GPU memory components here for speed.
@@ -131,7 +134,7 @@ class sim:
 
             # CUDA code for GPU, life function carries out the next step in CGOL and computes stablility factor for each cell.
             cudaCode = SourceModule("""
-            __global__ void run(const unsigned char* world, unsigned char* result, int* stable, const unsigned int side, const unsigned int size)
+            __global__ void run(const unsigned char* world, unsigned char* result, char* stable, const unsigned int side, const unsigned int size)
             {{
                 for (unsigned int cellLoc = __mul24(blockIdx.x, blockDim.x) + threadIdx.x;
                     cellLoc < size;
@@ -153,12 +156,26 @@ class sim:
 
                     /* Compute the result of the current state and stable factor for each cell. */
                     unsigned char prevState = world[cellLoc];
-                    unsigned char currState = (neighbors == 3) || (neighbors == 2 && prevState);
+                    unsigned char currState = (neighbors == 3) | (neighbors == 2 & prevState); /* Bitwise is faster. */
                     result[cellLoc] = currState;
-                    stable[cellLoc] = (currState * prevState * stable[cellLoc] + prevState * currState) + ({} * currState * (!prevState));
+   
+                    /* Stability function evaluation.
+                        newStabilityScore(cellTransition) =  
+                            1. alive --> alive do min(stabilityScore + 1, MAX_VALUE)
+                            2. dead  --> alive do MIN_VALUE
+                            3. otherwise       do 0 */
+                    if(currState & prevState)
+                        if(stable[cellLoc] != {})
+                            stable[cellLoc]++;
+                        else
+                            continue;
+                    else if(currState & !prevState)
+                        stable[cellLoc] = {};
+                    else
+                        stable[cellLoc] = 0;
                 }}
             }}
-            """.format(self.spawnStabilityFactor))
+            """.format(self.stableStabilityFactor, self.spawnStabilityFactor))
 
             # Originally used inverse and add 1 for mask, but switched to bit shift for efficiency.
             self.run_gpu = cudaCode.get_function('run')
@@ -203,7 +220,18 @@ class sim:
             prevState = self.world[cellLoc]
             currState = (neighbors == 3) or (neighbors == 2 and prevState)
             self.temp[cellLoc] = currState
-            self.stable[cellLoc] = (currState * prevState * self.stable[cellLoc] + prevState * currState) + (self.spawnStabilityFactor * currState * (not prevState))
+            # Stability function evaluation.
+            #    newStabilityScore(cellTransition) =  
+            #        1. alive --> alive do min(stabilityScore + 1, MAX_VALUE)
+            #        2. dead  --> alive do MIN_VALUE
+            #        3. otherwise       do 0
+            if (currState and prevState):
+                if (self.stable[cellLoc] != self.stableStabilityFactor):
+                    self.stable[cellLoc] += 1
+            elif(currState and (not prevState)):
+                self.stable[cellLoc] = self.spawnStabilityFactor
+            else:
+                self.stable[cellLoc] = 0
         self.world = np.copy(self.temp)
 
 # --- SIMULATOR ---
@@ -224,13 +252,19 @@ class sim:
         return np.add.reduce(self.world, dtype=np.uint64)  # Faster than np.sum() as of 7 APR 2024.
 
 # --- I/O ---
-    # Will either return a vector OR a 2D square matrix of the system.
-    def get_state(self, vector=False):
-        return self.world if vector else self.world.reshape((self.side, self.side))
+    # Will either return a vector OR a 2D square matrix of the system. NEED TO DO DEEP COPY!
+    def get_state(self, vector=False, shallow=False):
+        out = self.world if vector else self.world.reshape((self.side, self.side))
+        if shallow:
+            return out
+        return np.copy(out)
 
-    # Will either return a vector OR a 2D square matrix of the stables of each cell with respect to their position.
-    def get_stable(self, vector=False):
-        return self.stable if vector else self.stable.reshape((self.side, self.side))
+    # Will either return a vector OR a 2D square matrix of the stables of each cell with respect to their position. NEED TO DO DEEP COPY!
+    def get_stable(self, vector=False, shallow=False):
+        out = self.stable if vector else self.stable.reshape((self.side, self.side))
+        if shallow:
+            return out
+        return np.copy(out)
     
     # Returns the side length of the state.
     def get_side(self):
@@ -269,10 +303,10 @@ class sim:
     # Store all of the attributes of the current system.
     # state=None, side=8, seed=8, gpu=False, gpu_select=0, warp=8, spawnStabilityFactor=-1
     def save(self):
-        return (self.world, self.stable, self.side, self.count, self.spawnStabilityFactor)
+        return (self.world, self.stable, self.side, self.count, self.spawnStabilityFactor, self.stableStabilityFactor)
 
     # Load from memory an exact state setup.
-    def load(self, newState, newstable, side, count, spawnStabilityFactor):
+    def load(self, newState, newstable, side, count, spawnStabilityFactor, stableStabilityFactor):
 
         if not isinstance(newState, np.ndarray) or not isinstance(newstable, np.ndarray):
             raise TypeError('newState and newstable variables must be a Numpy ndarray!')
@@ -280,10 +314,15 @@ class sim:
             raise TypeError('side and count must be integer!')
         if side < 1 or count < 0:
             raise ValueError('side and count must be positive integers and side greater than 0!')
+        if not isinstance(spawnStabilityFactor, int):
+            raise TypeError('spawnStabilityFactor must be an integer!')
+        if not isinstance(stableStabilityFactor, int):
+            raise TypeError('stableStabilityFactor must be an integer!')
 
+        self.stableStabilityFactor = stableStabilityFactor
         self.spawnStabilityFactor = spawnStabilityFactor
         self.size = side ** 2
         self.side = side
         self.count = count
         self.world = newState.flatten().astype(np.uint8)
-        self.stable = newstable.flatten().astype(np.int32)
+        self.stable = newstable.flatten().astype(np.int8)
