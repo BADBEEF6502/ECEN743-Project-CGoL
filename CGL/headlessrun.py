b140663ad02f9d8a2cc932c6b2eb2ccb97d55c04
BADBEEF6502 <165059888+BADBEEF6502@users.noreply.github.com>
2024-04-25 18:22:27 -0500

    Used for testing headless operation on HPRC

---
+++ CGL/headlessrun.py
@@ -0,0 +1,21 @@
+# When you run this: GPU_CAPABLE='True' python3 headlessrun.py
+import CGL
+import numpy as np
+import time
+
+sample = np.array([[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 1, 1, 1, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]])
+                  
+cgl = CGL.sim(state=sample, gpu=True, spawnStabilityFactor=-2, stableStabilityFactor=2)
+start = time.perf_counter()
+RUN_ITERS = 1000000000
+
+for _ in range(RUN_ITERS):
+    print(_)
+    print(cgl.get_stable(),'\n')
+    cgl.step()
+print('Runtime=', time.perf_counter()-start)
+quit()
