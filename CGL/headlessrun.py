5752436e5321713cc8a7dd902529ab392db7530d
BADBEEF6502 <165059888+BADBEEF6502@users.noreply.github.com>
2024-04-25 19:26:40 -0500

    RUN_ITERS set to 1 million

--- CGL/headlessrun.py
+++ CGL/headlessrun.py
@@ -11,7 +11,7 @@ sample = np.array([[0, 0, 0, 0, 0],
                   
 cgl = CGL.sim(state=sample, gpu=True, spawnStabilityFactor=-2, stableStabilityFactor=2)
 start = time.perf_counter()
-RUN_ITERS = 1000000000
+RUN_ITERS = 1000000
 
 for _ in range(RUN_ITERS):
     print(_)
