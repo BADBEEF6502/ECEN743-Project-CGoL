75115ff937c72e59c81bd977e437d13f2795469a
Nicholas Heinrich-Barna <barna_nicholas@olympus.ece.tamu.edu>
2024-04-28 14:05:23 -0500

    added render update function

--- CGL/run.py
+++ CGL/run.py
@@ -9,12 +9,48 @@ from dqn import DQNAgent
 
 pygame.init()
 
+
+
+def render_update(env:CGL.sim,render_dict):
+    learner = DQNAgent(**kwargs)
+    epsilon = 0 
+    moving_window = deque(maxlen=100)
+    
+    for e in range(args.n_episodes):
+        env.reset()
+        state = env.get_state(vector=True)
+        
+        curr_reward = 0
+        for t in range(args.max_esp_len):
+            action = learner.select_action(state,epsilon) 
+            
+            env.toggle_state(action)
+            
+            env.step(action)
+            n_state     = env.get_state(vector=True)
+            reward      = env.reward()
+            
+            state = n_state
+            curr_reward += reward
+            
+        moving_window.append(curr_reward)
+        
+        if e % 10 == 0:
+            print('Episode Number {} Average Episodic Reward (over 100 episodes): {:.2f}'.format(e, np.mean(moving_window)))
+           
+
 # Color in32 is of the form: AARRGGBB (alpha, R, G, B).
 # If age is selected, you may see "pulsing" this is expect. This is because as the BB counts up in RRGGBB, it resests to zero and adds 1 to G, etc...
 def render(cgl, delay, dim, color=0xFF, showAge=False):
+    render_dict =   {
+                        ""
+                    }
+    
+    
+    
     display = pygame.display.set_mode(dim)
     running = True
-    side = cgl.get_side() 
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,60 +67,15 @@ def render(cgl, delay, dim, color=0xFF, showAge=False):
         pygame.display.update()
         #print(cgl.get_stable(), '\n')
         pygame.time.delay(delay)
-        cgl.step()
+        
+        render_update(cgl,render_dict)
 
     pygame.quit()
-
-
-if __name__ == "__main__":
-    # HyperParameters For DQN
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", default="LunarLander-v2")          # Gymnasium environment name
-    parser.add_argument("--seed", default=0, type=int)              # sets Gym, PyTorch and Numpy seeds
-    parser.add_argument("--n-episodes", default=2000, type=int)     # maximum number of training episodes
-    parser.add_argument("--batch-size", default=64, type=int)       # training batch size
-    parser.add_argument("--discount", default=0.99)                 # discount factor
-    parser.add_argument("--lr", default=5e-4)                       # learning rate
-    parser.add_argument("--tau", default=0.001)                     # soft update of target network
-    parser.add_argument("--max-size", default=int(1e5),type=int)    # experience replay buffer length
-    parser.add_argument("--update-freq", default=4, type=int)       # update frequency of target network
-    parser.add_argument("--gpu-index", default=0,type=int)          # GPU index
-    parser.add_argument("--max-esp-len", default=500, type=int)    # maximum time of an episode
-    #exploration strategy
-    parser.add_argument("--epsilon-start", default=1)               # start value of epsilon
-    parser.add_argument("--epsilon-end", default=0.01)              # end value of epsilon
-    parser.add_argument("--epsilon-decay", default=0.9965)#.995           # decay value of epsilon
-    args = parser.parse_args()
+        
+        
+        
+def train_model(env:CGL.sim,kwargs):   
     
-    
-
-    
-    
-
-
-    window_height = window_length = 500
-    delay_time = 0 # Milliseconds.
-    sim_side_size = 10
-    env = CGL.sim(side=sim_side_size, seed=1230, gpu=False, spawnStabilityFactor=-20, stableStabilityFactor=20)
-
-    state_dim = env.size
-    action_dim = env.size 
-
-    
-
-    kwargs = {
-        "state_dim":state_dim,
-        "action_dim":action_dim,
-        "discount":args.discount,
-        "tau":args.tau,
-        "lr":args.lr,
-        "update_freq":args.update_freq,
-        "max_size":args.max_size,
-        "batch_size":args.batch_size,
-        "gpu_index":args.gpu_index
-    }
-
-
     learner = DQNAgent(**kwargs)
     epsilon = args.epsilon_start 
     epsilon_decay = args.epsilon_decay
@@ -122,11 +113,6 @@ if __name__ == "__main__":
            
 
         data.append((e,np.mean(moving_window)))
-
-    quit()
-    #Visualization tool
-    render(env, delay=delay_time, dim=(window_height, window_length), showAge=True)
-
     episodes = []
     rewards = []
     
@@ -142,3 +128,66 @@ if __name__ == "__main__":
     plt.show()
     
     print('Runtime=', time.perf_counter()-start)
+    
+    
+def visualize_model():
+    pass
+    
+
+if __name__ == "__main__":
+    # HyperParameters For DQN
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="LunarLander-v2")          # Gymnasium environment name
+    parser.add_argument("--seed", default=0, type=int)              # sets Gym, PyTorch and Numpy seeds
+    parser.add_argument("--n-episodes", default=2000, type=int)     # maximum number of training episodes
+    parser.add_argument("--batch-size", default=64, type=int)       # training batch size
+    parser.add_argument("--discount", default=0.99)                 # discount factor
+    parser.add_argument("--lr", default=5e-4)                       # learning rate
+    parser.add_argument("--tau", default=0.001)                     # soft update of target network
+    parser.add_argument("--max-size", default=int(1e5),type=int)    # experience replay buffer length
+    parser.add_argument("--update-freq", default=4, type=int)       # update frequency of target network
+    parser.add_argument("--gpu-index", default=0,type=int)          # GPU index
+    parser.add_argument("--max-esp-len", default=500, type=int)    # maximum time of an episode
+    #exploration strategy
+    parser.add_argument("--epsilon-start", default=1)               # start value of epsilon
+    parser.add_argument("--epsilon-end", default=0.01)              # end value of epsilon
+    parser.add_argument("--epsilon-decay", default=0.9965)#.995           # decay value of epsilon
+    parser.add_argument("--run-type", default="visualize")
+    args = parser.parse_args()
+    
+    
+
+    
+    
+
+
+    window_height = window_length = 500
+    delay_time = 0 # Milliseconds.
+    sim_side_size = 10
+    env = CGL.sim(side=sim_side_size, seed=1230, gpu=False, spawnStabilityFactor=-20, stableStabilityFactor=20)
+
+    state_dim = env.size
+    action_dim = env.size 
+
+    
+
+    kwargs = {
+        "state_dim":state_dim,
+        "action_dim":action_dim,
+        "discount":args.discount,
+        "tau":args.tau,
+        "lr":args.lr,
+        "update_freq":args.update_freq,
+        "max_size":args.max_size,
+        "batch_size":args.batch_size,
+        "gpu_index":args.gpu_index
+    }
+
+
+    if "train" in args.run_type:
+        train_model(env,kwargs)
+        
+    elif "visualize" in args.run_type:
+        render(env, delay=delay_time, dim=(window_height, window_length), showAge=True)
+
+    
